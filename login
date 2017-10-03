0f3d28c29a2fe65e59627c94dd5857fccde4e586
Georgios Gousios <gousiosg@bdp1.ewi.tudelft.nl>
2017-10-03 23:20:03 +0200

    Remove not used stuff

--- login
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-tmux new-session "tmux source-file /home/pi/cluster/tmux/tmux-login.conf"
--- poweroff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for i in `seq 1 4`;
-do
-  echo "Shutting down slave$i"
-  ssh "slave$i.spark" "sudo poweroff"
-done
-
-sudo poweroff
