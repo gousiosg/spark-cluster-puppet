ee02ddca7a64f7d091004a5b3808502026cf4d6f
Georgios Gousios <gousiosg@bdp1.ewi.tudelft.nl>
2017-10-03 23:21:33 +0200

    Fix working dir for rsync

--- sync-cluster.sh
+++ sync-cluster.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-for h in `cat files/slaves`; do rsync --delete -av cluster/ $h:~/cluster/; done
+for h in `cat files/slaves`; do rsync --delete -av /home/gousiosg/cluster/ $h:~/cluster/; done
 for h in `cat files/slaves`; do ssh -t -t -o StrictHostKeyChecking=no $h 'sudo  puppet apply --modulepath=/home/gousiosg/cluster/modules /home/gousiosg/cluster/nodes.pp'  ; done
